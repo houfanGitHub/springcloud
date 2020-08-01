c59fd6c0aad93b44624463fc3488b741ba79b994
houfanGitHub <354885789@qq.com>
2020-08-01 18:11:21 +0800

    spring cloud bus

--- eureka-client/src/main/java/com/example/eurekaclient/contorller/IndexController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/contorller/IndexController.java
@@ -3,11 +3,13 @@ package com.example.eurekaclient.contorller;
 import com.example.eurekaclient.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class IndexController {
 
     @Autowired
